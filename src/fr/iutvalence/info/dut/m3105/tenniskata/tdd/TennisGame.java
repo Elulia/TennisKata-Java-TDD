b12dd75f9937c3d6b52b27d0e6950de4b5f34afb
combear <arthur.combe@iut-valence.fr>
2015-10-20 13:12:30 +0200

    TEST 2 GREEN

--- src/fr/iutvalence/info/dut/m3105/tenniskata/tdd/TennisGame.java
+++ src/fr/iutvalence/info/dut/m3105/tenniskata/tdd/TennisGame.java
@@ -4,20 +4,22 @@ import org.junit.Assert;
 
 public class TennisGame
 {
-	private int score1 =0;
-	private int score2 =0;
+	private String score;
+	
+	
+	public TennisGame()
+	{	
+		this.score = "Love-All";
+	}
 
 	public String getScore() 
 	{
-		if (score1 == 15){
-			return "Fifteen-Love";
-		}
-		return "Love-All";
+		return this.score;
 	}
-
 	
 	public void serverScore()
 	{
-		this.score1 += 15;
+		this.score = "Fifteen-Love";
 	}
 }
+
--- test/fr/iutvalence/info/dut/m3105/tenniskata/tdd/TennisGameTest.java
+++ test/fr/iutvalence/info/dut/m3105/tenniskata/tdd/TennisGameTest.java
@@ -1,24 +1,26 @@
 package fr.iutvalence.info.dut.m3105.tenniskata.tdd;
 
-
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class TennisGameTest
 {
+	
+	
+	
 	@Test
-	public void firstTestGetScore()
+	public void whenGameHasJustStartedThenScoreShouldBe_love_all()
 	{
-		TennisGame tennisGame= new TennisGame();
+		TennisGame tennisGame = new TennisGame();
 		Assert.assertEquals(tennisGame.getScore(), "Love-All");
 	}
 	
 	@Test
-	public void secondTestGetScore()
+	public void isServerHasScoredOnTimeAndReceiverHasNotScoredThenScoreShouldBe_Fifteen_Love()
 	{
-		TennisGame tennisGame= new TennisGame();
+		TennisGame tennisGame = new TennisGame();
 		tennisGame.serverScore();
 		Assert.assertEquals(tennisGame.getScore(), "Fifteen-Love");
-		
 	}
 }
