0f09421db42cd334f275cea23270b5b7dbd3e65a
combear <arthur.combe@iut-valence.fr>
2015-10-20 12:53:01 +0200

    test 1 (Green 1)

--- src/fr/iutvalence/info/dut/m3105/tenniskata/tdd/TennisGame.java
+++ src/fr/iutvalence/info/dut/m3105/tenniskata/tdd/TennisGame.java
@@ -9,7 +9,11 @@ public class TennisGame
 
 	public static String getScore(int score1, int score2) 
 	{
-			
+		if (score1 == 0 && score2 == 0)
+		{
+			return "Love-All";
+		}
+		
 		return null;
 	}
 	
